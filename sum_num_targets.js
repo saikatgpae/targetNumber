4c383b5a34cd627080a9e4d0e3bf4197286523ce
Saikat Chakraborty <saikatgpae@gmail.com>
2023-01-30 21:13:04 +0530

    Solution.

--- sum_num_targets.js
+++ sum_num_targets.js
@@ -1,16 +1,16 @@
-// This function return the index of the 2 numbers
+// This function return the 2 numbers
 
 var twoSum = function(nums, target) {
     var arr = []
     for (let i=0; i<nums.length; i++){
       for (let j=0; j<nums.length; j++){
           if(nums[i] + nums[j] === target && i !== j){
-            //   console.log(i,j)
-            if(!arr.includes(i)){
-                arr.push(i);
+            //   console.log(nums[i],nums[j])
+            if(!arr.includes(nums[i])){
+                arr.push(nums[i]);
             }
-            if(!arr.includes(j)){
-                arr.push(j);
+            if(!arr.includes(nums[j])){
+                arr.push(nums[j]);
             }
           }
       }
@@ -19,13 +19,13 @@ var twoSum = function(nums, target) {
     console.log(arr);
 };
 
-const nums = [2,7,11,15];
-const target = 9;
+// const nums = [2,7,11,15];
+// const target = 9;
 
 // const nums = [3,2,4];
 // const target = 6;
 
-// const nums = [3,3];
-// const target = 6;
+const nums = [3,3];
+const target = 6;
 
 twoSum(nums, target)
